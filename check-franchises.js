76d57bf9d3117d987a750afcc7a19294c010ecc8
maxr-hw <maxr.hw@gmail.com>
2026-03-11 10:22:22 +0100

    clean up

--- check-franchises.js
+++
@@ -1,3 +0,0 @@
-const db = require('better-sqlite3')('legomarket.db');
-const rows = db.prepare("SELECT franchise, COUNT(*) as n FROM sets WHERE set_num IN (SELECT set_num FROM deals) GROUP BY franchise ORDER BY n DESC LIMIT 30").all();
-rows.forEach(r => console.log(`${r.n.toString().padStart(4)} ${r.franchise}`));
--- check-others.js
+++
@@ -1,5 +0,0 @@
-const db = require('better-sqlite3')('legomarket.db');
-const rows = db.prepare(
-  "SELECT name, theme_name, franchise, set_num FROM sets WHERE franchise = 'other' AND set_num IN (SELECT set_num FROM deals) ORDER BY RANDOM() LIMIT 60"
-).all();
-rows.forEach(r => console.log(r.franchise, '|', r.theme_name, '|', r.name.substring(0, 70)));
